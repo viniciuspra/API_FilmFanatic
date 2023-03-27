e286670633c9ded766744bea41be42b183bfcf8c
viniciuspra <93842439+viniciuspra@users.noreply.github.com>
2023-03-27 20:38:36 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 require('express-async-errors');
+const database = require('./database/sqlite')
 const AppError = require('./utils/AppError')
 const express = require('express');
 const routes = require('./routes');
@@ -6,6 +7,8 @@ const routes = require('./routes');
 const app = express();
 app.use(express.json())
 app.use(routes);
+
+database();
 
 app.use((error, request, response, next) => {
   if (error instanceof AppError) {
