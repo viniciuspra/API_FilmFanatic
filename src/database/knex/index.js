f72edad03f1ff2b4dba96c8e8e5c9d578ad10e34
viniciuspra <93842439+viniciuspra@users.noreply.github.com>
2023-03-30 18:49:45 -0300

    Update index.js

--- src/database/knex/index.js
+++ src/database/knex/index.js
@@ -1,6 +1,6 @@
-const config = require('../../../knexfile');
-const knex = require('knex');
+const config = require("../../../knexfile")
+const knex = require("knex")
 
-const connection = knex(config.connection);
+const connection = knex(config.development)
 
-module.exports = connection;
+module.exports = connection
