eb7267bf21c059bf31c5f6522e39eb0da81bf4dc
viniciuspra <93842439+viniciuspra@users.noreply.github.com>
2023-04-03 20:01:24 -0300

    Update notes.routes.js

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -3,7 +3,9 @@ const NotesController = require("../Controllers/NotesController")
 const notesRoutes = Router()
 const notesController = new NotesController()
 
+notesRoutes.get("/", notesController.index)
 notesRoutes.post("/:user_id", notesController.create)
 notesRoutes.get("/:id", notesController.show)
+notesRoutes.delete("/:id", notesController.delete)
 
 module.exports = notesRoutes
