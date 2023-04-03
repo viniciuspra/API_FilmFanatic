cc833add887d1808e8ef71f922e0a8045f37c519
viniciuspra <93842439+viniciuspra@users.noreply.github.com>
2023-04-03 16:33:32 -0300

    Update NotesController.js

--- src/Controllers/NotesController.js
+++ src/Controllers/NotesController.js
@@ -29,6 +29,15 @@ class NotesController {
 
     return response.json();
 
-}};
+  }
+
+  async show(request, response) {
+    const { id } = request.params;
+
+    const note = await knex("movie_notes").where({id}).first();
+
+    return response.json(note);
+  }
+};
 
 module.exports = NotesController;
