7f8db0974711365fd4556269b9b465aeb8c6c6be
viniciuspra <93842439+viniciuspra@users.noreply.github.com>
2023-04-03 16:33:34 -0300

    Update notes.routes.js

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -1,8 +1,9 @@
 const { Router } = require("express")
-const NotesController = require("../controllers/NotesController")
+const NotesController = require("../Controllers/NotesController")
 const notesRoutes = Router()
 const notesController = new NotesController()
 
 notesRoutes.post("/:user_id", notesController.create)
+notesRoutes.get("/:id", notesController.show)
 
 module.exports = notesRoutes
