4c44d4f9812de20ab624865338e9fb014cd1b767
viniciuspra <93842439+viniciuspra@users.noreply.github.com>
2023-04-03 20:01:20 -0300

    Update NotesController.js

--- src/Controllers/NotesController.js
+++ src/Controllers/NotesController.js
@@ -34,9 +34,42 @@ class NotesController {
   async show(request, response) {
     const { id } = request.params;
 
-    const note = await knex("movie_notes").where({id}).first();
+    const note = await knex("movie_notes").where({ id }).first();
+    const tags = await knex("movie_tags").where({ note_id: id }).orderBy("name");
 
-    return response.json(note);
+    return response.json({
+      ...note,
+      tags
+    });
+  }
+
+  async delete(request, response) {
+    const { id } = request.params;
+
+    await knex("movie_notes").where({ id }).delete();
+
+    return response.json();
+  }
+
+  async index(request, response) {
+    const { title, user_id, tags } = request.query;
+
+    let notes;
+
+    if (tags) {
+      const filterTags = tags.split(',').map(tag => tag.trim());
+
+      notes = await knex("movie_tags").whereIn("name", filterTags)
+
+    }else {
+      notes = await knex("movie_notes")
+        .where({ user_id })
+          .whereLike("title", `%${title}%`)
+            .orderBy("title");
+    }
+
+
+    return response.json(notes)
   }
 };
 
