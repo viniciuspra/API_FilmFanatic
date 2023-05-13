69e4dcae14dd3bb756c9fde57ed3c6a2c140b085
viniciuspra <93842439+viniciuspra@users.noreply.github.com>
2023-05-13 18:14:20 -0300

    Implement image upload for user avatars

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,11 +1,20 @@
 const { Router } = require('express');
+const multer = require('multer');
+
+const uploadConfig = require('../configs/upload');
+const upload = multer(uploadConfig.MULTER)
+
 const UsersController = require('../Controllers/UsersController');
 const usersController = new UsersController();
-const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
 const usersRouter = Router();
 
 usersRouter.post("/", usersController.create);
 usersRouter.put("/", ensureAuthenticated, usersController.update);
+usersRouter.patch("/avatar", ensureAuthenticated, upload.single('avatar'), (request, response) => {
+  console.log(request.file.filename);
+  response.json()
+})
 
 module.exports = usersRouter;
