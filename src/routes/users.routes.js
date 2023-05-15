0621ca80f4a0fc23955c85f6644d2fd0903f1c10
viniciuspra <93842439+viniciuspra@users.noreply.github.com>
2023-05-15 19:14:42 -0300

    Save uploaded user avatars to the database

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -5,16 +5,16 @@ const uploadConfig = require('../configs/upload');
 const upload = multer(uploadConfig.MULTER)
 
 const UsersController = require('../Controllers/UsersController');
-const usersController = new UsersController();
+const UserAvatarController = require("../Controllers/UserAvatarController");
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
+
+const usersController = new UsersController();
+const userAvatarController = new UserAvatarController()
 
 const usersRouter = Router();
 
 usersRouter.post("/", usersController.create);
 usersRouter.put("/", ensureAuthenticated, usersController.update);
-usersRouter.patch("/avatar", ensureAuthenticated, upload.single('avatar'), (request, response) => {
-  console.log(request.file.filename);
-  response.json()
-})
+usersRouter.patch("/avatar", ensureAuthenticated, upload.single("avatar"), userAvatarController.update)
 
 module.exports = usersRouter;
--- src/server.js
+++ src/server.js
@@ -3,10 +3,12 @@ const migrationsRun = require("./database/sqlite/migrations");
 const AppError = require("./utils/AppError");
 const express = require("express");
 const routes = require("./routes");
+const cors = require("cors");
 
-const app = express();
 migrationsRun();
 
+const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use(routes);
