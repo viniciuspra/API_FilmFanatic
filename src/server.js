eeef2896b69af69f85d026d1ab1153b93aac8956
viniciuspra <93842439+viniciuspra@users.noreply.github.com>
2023-06-27 19:05:56 -0300

    Add dotenv and .env files

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 require("express-async-errors")
+require("dotenv/config")
+
 const migrationsRun = require("./database/sqlite/migrations")
 const uploadConfig = require("./configs/upload")
 const AppError = require("./utils/AppError")
@@ -32,6 +34,6 @@ app.use((error, request, response, next) => {
   })
 })
 
-const PORT = 3333
+const PORT = process.env.PORT || 3333
 
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
